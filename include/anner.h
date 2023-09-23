357c44f31bb01e9baff8932b3cfc792e2f8ace96
ZiHan Huang <zack.huang@rock-chips.com>
2023-09-23 15:43:30 +0800

    include: distinguish off-screen rendering dummy function
    Signed-off-by: ZiHan Huang <zack.huang@rock-chips.com>

--- include/anner.h
+++ include/anner.h
@@ -4,13 +4,15 @@
 
 void anner_create_window(int window_width, int window_height);
 int anner_create_texture(unsigned char* pixels, int w, int h, int format);
-int anner_create_intput(void** pixels, int *drmbuf_fd, int w, int h, int format, int stride);
-int anner_create_output(void** pixels, int *drmbuf_fd, int w, int h, int format, int stride);
-void anner_activation_texture(void* pixels, int drmbuf_fd, int w, int h, int format, int stride);//dummy
-int anner_disable_texture();
 int anner_delete_texture(void);
-int anner_delete_buf(void* pixels, int drm_fd, int len, int type);
 void anner_destory_window(void);
-void anner_set_effects(int Angle);
 void anner_render(int w, int h);
 int anner_dumpPixels(int len, int inWindowWidth, int inWindowHeight, unsigned char * pPixelDataFront, char* file_name);
+
+//Off-screen rendering dummy function
+int anner_create_intput(void** pixels, int *drmbuf_fd, int w, int h, int format, int stride);
+int anner_create_output(void** pixels, int *drmbuf_fd, int w, int h, int format, int stride);
+void anner_activation_texture(void* pixels, int drmbuf_fd, int w, int h, int format, int stride);
+int anner_disable_texture();
+int anner_delete_buf(void* pixels, int drm_fd, int len, int type);
+void anner_set_effects(int Angle);
